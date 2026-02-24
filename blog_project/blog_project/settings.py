acc623740db976ad84142e7bae344b68285ff94f
Sagar Raturi <sagarraturi85516@gmail.com>
2026-02-24 16:47:16 +0530

    Add Login & Logout system

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -128,3 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'booststrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
